eed26eee839ea565d6e3375cb4b499b8e7c36002
raphavtorres <raphaeltorresprofissional@gmail.com>
2023-09-28 08:14:46 -0300

    feat: registered models in admin

--- app/admin.py
+++ app/admin.py
@@ -2,18 +2,114 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomerCreateForm, CustomerChangeForm
-from .models import Customer
+from .models import (
+    Customer,
+    NaturalPerson,
+    LegalPerson,
+    Email,
+    Phone,
+    Address,
+    Account,
+    Investment,
+    Loan,
+    Installment,
+    Card,
+    Transaction
+)
 
 
 @admin.register(Customer)
 class CustomerAdmin(UserAdmin):
-  add_form = CustomerCreateForm
-  form = CustomerChangeForm
-  model = Customer
-  list_display = ('register_number', 'first_name', 'last_name', 'is_staff')
-  fieldsets = (
-    (None, {'fields': ('email', 'password')}),
-    ('Personal Information', {'fields': ('first_name', 'last_name')}),
-    ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-    ('Important Dates', {'fields': ('last_login', 'date_joined')})
-  )
+    add_form = CustomerCreateForm
+    form = CustomerChangeForm
+    model = Customer
+    list_display = ('register_number', 'first_name', 'last_name', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Information', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff',
+                                    'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')})
+    )
+
+
+@admin.register(NaturalPerson)
+class NaturalPersonAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'birthdate',
+        'cpf',
+        'rg',
+        'social_name'
+    ]
+
+    ordering = ['id']
+    # list_filter = ['']
+
+
+@admin.register(LegalPerson)
+class LegalPersonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer',
+                    'fantasy_name', 'establishment_date', 'cnpj', 'im', 'ie', 'legal_nature']
+    ordering = ['id']
+
+
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'customer']
+    ordering = ['id']
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'phone', 'country_code', 'prefix_number']
+    ordering = ['id']
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'neighborhood',
+                    'street', 'number', 'city', 'state', 'cep']
+    ordering = ['id']
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number', 'agency',
+                    'acc_type', 'credit_limit', 'is_active']
+    ordering = ['id']
+
+
+@admin.register(Investment)
+class InvestmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'id_account', 'investment_type', 'contribution',
+                    'income', 'admin_fee', 'period', 'risc_rate', 'profitability']
+    ordering = ['id']
+
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ['id', 'id_account', 'amount_request', 'interest_rate', 'is_payout',
+                    'installment_amount', 'request_date', 'approval_date', 'observation']
+    ordering = ['id']
+
+
+@admin.register(Installment)
+class InstallmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'loan', 'number',
+                    'payment_amount', 'payment_date', 'expiration_date']
+    ordering = ['id']
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ['id', 'account', 'number',
+                    'verification_code', 'flag', 'expiration_date', 'is_active']
+    ordering = ['id']
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'card', 'amount', 'transaction_type', 'timestamp']
+    ordering = ['id']
---
+++ app/populate.py
@@ -0,0 +1,8 @@
+import requests
+import os
+
+
+# BASE_URL = 'http://127.0.0.1:8000/api/v1/'
+
+# <endpoint>_url = os.path.join(BASE_URL,'<endpoints>/')
+# ...
