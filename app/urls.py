b62cd4f2dea1c1d30bbc14b521828c07fd6eb6f8
raphavtorres <raphaeltorresprofissional@gmail.com>
2023-10-05 09:46:00 -0300

    feat: created views

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,33 @@ from django.urls import path
 
 from rest_framework.routers import SimpleRouter
 
-# from .views import
+from .views import (
+    NaturalPersonViewSet,
+    LegalPersonViewSet,
+    EmailViewSet,
+    PhoneViewSet,
+    AddressViewSet,
+    AccountViewSet,
+    InvestmentViewSet,
+    LoanViewSet,
+    InstallmentViewSet,
+    CardViewSet,
+    TransactioViewSet
+)
+
+
+router = SimpleRouter()
+router.register('natural-people', NaturalPersonViewSet)
+router.register('legal-people', LegalPersonViewSet)
+router.register('emails', EmailViewSet)
+router.register('phones', PhoneViewSet)
+router.register('addresses', AddressViewSet)
+router.register('accounts', AccountViewSet)
+router.register('investments', InvestmentViewSet)
+router.register('loans', LoanViewSet)
+router.register('installments', InstallmentViewSet)
+router.register('cards', CardViewSet)
+router.register('transactions', TransactioViewSet)
+
+
+urlpatterns = []
--- app/views.py
+++ app/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import (
+    NaturalPerson,
+    LegalPerson,
+    Email,
+    Phone,
+    Address,
+    Account,
+    Investment,
+    Loan,
+    Installment,
+    Card,
+    Transaction,
+)
+
+from .serializers import (
+    NaturalPersonSerializer,
+    LegalPersonSerializer,
+    EmailSerializer,
+    PhoneSerializer,
+    AddressSerializer,
+    AccountSerializer,
+    InvestmentSerializer,
+    LoanSerializer,
+    InstallmentSerializer,
+    CardSerializer,
+    TransactionSerializer,
+)
+
+
+class NaturalPersonViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+
+    queryset = NaturalPerson.objects.all()
+    serializer_class = NaturalPersonSerializer
+
+
+class LegalPersonViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+
+    queryset = LegalPerson.objects.all()
+    serializer_class = LegalPersonSerializer
+
+
+class EmailViewSet(viewsets.ModelViewSet):
+    queryset = Email.objects.all()
+    serializer_class = EmailSerializer
+
+
+class PhoneViewSet(viewsets.ModelViewSet):
+    queryset = Phone.objects.all()
+    serializer_class = PhoneSerializer
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class InvestmentViewSet(viewsets.ModelViewSet):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+
+class InstallmentViewSet(viewsets.ModelViewSet):
+    queryset = Installment.objects.all()
+    serializer_class = InstallmentSerializer
+
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
