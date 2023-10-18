90d4470267439515e95655544aa74263e5282bc8
raphavtorres <raphaeltorresprofissional@gmail.com>
2023-10-18 10:48:20 -0300

    refactor: update routes

--- app/urls.py
+++ app/urls.py
@@ -2,30 +2,10 @@ from .imports.url import *
 
 
 router = SimpleRouter()
-# NATURAL PERSON
-router.register(
-  'natural-people', 
-  NaturalPersonGetViewSet, 
-  basename='natural-people'
-)
-router.register(
-  'natural-people-create'
-  , NaturalPersonPostViewSet, 
-  basename='natural-people-create'
-)
 
-# LEGAL PERSON
-router.register(
-  'legal-people', 
-  LegalPersonGetViewSet,
-  basename='legal-people'
-)
-router.register(
-  'legal-people-create', 
-  LegalPersonPostViewSet,
-  basename='legal-people-create'
-)
-
+router.register('natural-people', NaturalPersonViewSet,
+                basename='natural-people')
+router.register('legal-people', LegalPersonViewSet, basename='legal-people')
 router.register('emails', EmailViewSet)
 router.register('phones', PhoneViewSet)
 router.register('addresses', AddressViewSet)
